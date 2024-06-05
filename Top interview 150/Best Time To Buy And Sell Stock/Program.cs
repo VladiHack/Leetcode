c5a76e0791b780be099fea13f149210bbcbc675d
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-06-05 14:54:13 +0300

    Best Time To Buy And Sell Stock

---
+++ Top interview 150/Best Time To Buy And Sell Stock/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Console.WriteLine(MaxProfit(arr));
+    }
+    public static int MaxProfit(int[] prices)
+    {
+        int profit = 0;int smallestByFar = int.MaxValue;
+        for(int i = 0; i < prices.Length-1; i++)
+        {
+            if (prices[i] >= prices[i+1])
+            {
+                continue;
+            }
+            if (prices[i]<smallestByFar)
+            {
+                smallestByFar = prices[i];
+                for(int j=i+1;j<prices.Length;j++)
+                {
+                    if (prices[j] - prices[i] > profit)
+                    {
+                        profit = prices[j] - prices[i];
+                    }
+                }
+            }
+        }
+        return profit;
+    }
+}
+
