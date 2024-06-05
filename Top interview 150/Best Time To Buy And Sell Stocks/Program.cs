31dc087b1398c33708c3e18c8c8f10ce8713c93a
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-06-05 16:49:01 +0300

    Sell Stocks II

---
+++ Top interview 150/Best Time To Buy And Sell Stocks/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Intrinsics.Arm;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] arr = Console.ReadLine().Split(",").Select(int.Parse).ToArray();
+        Console.WriteLine(MaxProfit(arr));
+    }
+    public static int MaxProfit(int[] prices)
+    {
+        int profit = 0;
+
+        for (int i = 1; i < prices.Length; i++)
+        {
+            if (prices[i] > prices[i - 1])
+                profit += prices[i] - prices[i - 1];
+        }
+
+        return profit;
+    }
+   
+}
