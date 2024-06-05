818f1402c25c2b6d8c61e3465993c1b77a3e9fd3
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-06-05 13:06:41 +0300

    Remove Duplicates From Array II

---
+++ Top interview 150/Remove Duplicates From Sorted Array II/Program.cs
@@ -0,0 +1,70 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        RemoveDuplicates(nums);
+        Console.WriteLine(String.Join(",",nums));
+    }
+    public static int RemoveDuplicates(int[] nums)
+    {
+        const int max = int.MaxValue;
+        int remaining = 0; int currentNum = max; bool repeated = false;
+        for(int i=0;i<nums.Length; i++)
+        {
+            if (nums[i] == currentNum)
+            {
+                if (repeated)
+                {
+                    nums[i] = max;
+                }
+                else
+                {
+                    remaining++;
+                    repeated = true;
+                }
+            }
+            else
+            {
+                currentNum = nums[i];
+                remaining++;
+                repeated = false;
+            }
+        }
+        int lastK = -1;
+        for (int i = 0; i < remaining; i++)
+        {
+            if (nums[i]==max)
+            {
+                if (lastK == -1)
+                {
+                    for (int k = i + 1; k < nums.Length; k++)
+                    {
+                        if (nums[k] != max)
+                        {
+                            int temp = nums[k];
+                            nums[k] = nums[i];
+                            nums[i] = temp;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    for (int k = lastK; k < nums.Length; k++)
+                    {
+                        if (nums[k] != max)
+                        {
+                            int temp = nums[k];
+                            nums[k] = nums[i];
+                            nums[i] = temp;
+                            break;
+                        }
+                    }
+                }
+                        
+            }
+        }
+        return remaining;
+    }
+}
