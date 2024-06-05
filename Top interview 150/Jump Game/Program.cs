320f632039feaca6836c52ed0775fb5f809560b4
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-06-05 18:34:03 +0300

    Jump Game

---
+++ Top interview 150/Jump Game/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Program
+{
+    static bool[] visited = new bool[10001]; static bool canJump = false;
+    public static void Main(string[] args)
+    {
+        int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Console.WriteLine(CanJump(arr));
+    }
+    public static bool CanJump(int[] nums)
+    {
+        CheckVariations(nums, 0);
+        return visited[nums.Length-1];
+    }
+    public static void CheckVariations(int[] nums,int index)
+    {
+        if(visited[index])
+        {
+            return;
+        }
+        visited[index] = true;
+        for(int i = 1; i <= nums[index];i++)
+        {
+            if (index + i >= nums.Length)
+            {
+                break;
+            }
+            CheckVariations(nums, index + i);
+        }
+    }
+}
