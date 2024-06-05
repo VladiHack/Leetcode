ea78694100b46c53d99f3fe2e452b445a0d0733e
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-06-05 14:13:31 +0300

    Rotate Array

---
+++ Top interview 150/Rotate Array/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int k = int.Parse(Console.ReadLine());
+        Rotate(arr,k);
+        Console.WriteLine(string.Join(" ",arr));
+    }
+    public static void Rotate(int[] nums, int k)
+    {
+        int[] arr = new int[nums.Length];
+        for(int i=0;i<nums.Length;i++)
+        {
+            if(i+k>=nums.Length)
+            {
+                arr[(i+k)%nums.Length]= nums[i];
+            }
+            else
+            {
+                arr[i + k] = nums[i];
+            }
+        }
+        for(int i=0;i<arr.Length;i++)
+        {
+            nums[i] = arr[i];
+        }
+    }
+}
