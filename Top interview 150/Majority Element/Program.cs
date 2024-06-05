72ee371c50574d39ccd95926a1e4508d9d0f2cc0
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-06-05 13:53:56 +0300

    Majority Element

---
+++ Top interview 150/Majority Element/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Console.WriteLine(MajorityElement(arr));
+    }
+    public static int MajorityElement(int[] nums)
+    {
+        Dictionary<int,int> timesMet=new Dictionary<int,int>();
+        int mostTimesMet = 0;
+        int mostMetNum = 0;
+        for(int i=0;i<nums.Length;i++)
+        {
+            if (!timesMet.ContainsKey(nums[i]))
+            {
+                timesMet.Add(nums[i],0);
+            }
+            timesMet[nums[i]]++;
+            if (timesMet[nums[i]] > mostTimesMet)
+            {
+                mostTimesMet = timesMet[nums[i]];
+                mostMetNum = nums[i];
+            }
+        }
+        return mostMetNum;
+    }
+}
