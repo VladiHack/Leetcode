e1f6f2d5bcc9c1a6f7a96bfb6fcd449fad6c3a86
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-06-05 12:15:19 +0300

    Remove Duplicates From Sorted Array

---
+++ Top interview 150/Remove Duplicates from Sorted Array/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] nums=Console.ReadLine().Split().Select(int.Parse).ToArray();
+        RemoveDuplicates(nums);
+        Console.WriteLine(String.Join(",",nums));
+    }
+    public static int RemoveDuplicates( int[] nums)
+    {
+        int[] arr = new int[nums.Length];int index = 0;
+        if(nums.Length> 0)
+        {
+            arr[index] = nums[index];
+            index++;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[i-1])
+                {
+                    arr[index] = nums[i];
+                    index++;
+                }
+            }
+            for(int i=0; i < index; i++)
+            {
+                nums[i] = arr[i];
+            }
+        }
+        
+        return index;
+    }
+}
