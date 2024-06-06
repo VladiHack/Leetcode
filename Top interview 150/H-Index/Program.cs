18b4ee049b1211283a68b7ba2be9d9718c4e969e
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-06-06 14:30:09 +0300

    HIndex

---
+++ Top interview 150/H-Index/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] citations=Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Console.WriteLine(HIndex(citations));
+    }
+    public static int HIndex(int[] citations)
+    {
+        int n = citations.Length;
+        Array.Sort(citations);
+        for (int i = 0; i < n; i++)
+        {
+            if (citations[i] >= n - i) return n - i;
+        }
+        return 0;
+    }
+}
