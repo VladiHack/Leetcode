2497c78dfcc6b7a793e98a744188a9821b3d65fd
VladiHack <113020161+VladiHack@users.noreply.github.com>
2024-06-07 15:59:34 +0300

    Product of Array Except Self

---
+++ Top interview 150/Product of Array Except Self/Program.cs
@@ -0,0 +1,44 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Console.WriteLine(String.Join(",",ProductExceptSelf(nums)));
+
+    }
+    public static int[] ProductExceptSelf(int[] nums)
+    {
+        int product = 1;int timesHasZero = 0;
+        for(int i=0;i<nums.Length; i++)
+        {
+            if (nums[i]==0&&timesHasZero==0)
+            {
+                timesHasZero++;
+            }
+            else
+            {
+                product *= nums[i];
+            }
+        }
+        for(int i=0;i<nums.Length;i++)
+        {
+            if(timesHasZero==2)
+            {
+                nums[i] = 0;
+            }
+            else if (nums[i]==0)
+            {
+                nums[i] = product;
+            }
+            else if(timesHasZero==0)
+            {
+                nums[i] = product / nums[i];
+            }
+            else
+            {
+                nums[i] = 0;
+            }
+        }
+        return nums;
+    }
+}
